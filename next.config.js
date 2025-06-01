fde4d7b49452c63ac5768e20473aae32ca56b2d1
Sylvain <sylvain.maurier@gmail.com>
2025-06-01 12:10:32 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  output: 'export',
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
